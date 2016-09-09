d4184fd5014c32f3d4b85a1a4314c419dfe7dd28
Benjamin Bengfort <benjamin@bengfort.com>
2016-09-09 17:49:28 -0400

    dealt with graphml writing issues

--- sklearn_graph.py
+++ sklearn_graph.py
@@ -13,50 +13,128 @@
 #############################################################################
 # Imports
 #############################################################################
+
 import os
 import csv
-import datetime
 import networkx as nx
 import matplotlib.pyplot as plt
+
+from functools import partial
+from datetime import datetime
 from operator import itemgetter
+from collections import namedtuple
+
 
 #############################################################################
 # Fixtures
 #############################################################################
-INPATH     = 'sklearncommits_mini.txt' # full set was slow so clipped last 2yrs
+
+DATA       = os.path.join(os.path.dirname(__file__), 'data')
+INPATH     = os.path.join(DATA, 'sklearncommits.txt')
 IMGPATH    = 'sklearngraph.png'
 GRAPHML    = 'sklearngraph.graphml'
-DATEFORMAT = '%a %b %d %I:%M:%S %Y %z'
 
-g = nx.Graph(name='Sklearn Commits')
+DATEFORMAT       = '%a %b %d %H:%M:%S %Y %z'
+ISO8601_DATETIME = "%Y-%m-%dT%H:%M:%S%z"
 
-ifile   = open(os.path.join('data', INPATH), 'r')
-commits  = csv.reader(ifile)
+Commit = namedtuple('Commit', 'commit, parents, contributor, timestamp, elapsed')
 
-for commit in commits:
-    commit_hash = commit[0]     # Uniquely identifies a commit
-    parent_hashes = commit[1]
-    contributor = commit[2]
-    try: 
-        commit_timestamp = datetime.datetime.strptime(commit[3], DATEFORMAT).date()
-    except:
-        pass
 
-    g.add_node(commit_hash, timestamp=commit_timestamp)  # add other elements?
-    g.add_node(contributor)
-    g.add_edge(contributor, commit_hash, label='contributor')
+def parse_dataset(path):
+    """
+    Reads the data set and yields parsed tuples
+    """
+    with open(path, 'r') as ifile:
+        # Create the CSV reader
+        reader = csv.reader(ifile, quotechar='"')
+        for row in reader:
 
-    for parent in parent_hashes:
-        g.add_node(parent, timestamp=commit_timestamp)
-        delta = g.node[parent]['timestamp']-g.node[commit_hash]['timestamp']
-        g.add_edge(parent, commit_hash, label='parent', weight=delta.total_seconds())
+            # Wrangle data
+            try:
+                row = list(map(lambda i: i.strip(), row))
+                row[1] = row[1].split(" ")
+                row[3] = datetime.strptime(row[3], DATEFORMAT)
 
-print("Nodes of graph: ")
-print(g.nodes())
-print("Edges of graph: ")
-print(g.edges())
+                if len(row) > 5:
+                    row[4] = ", ".join(row[4:])
+                    row = row[:5]
 
-nx.draw(g)
-plt.show()
-# plt.savefig(os.path.join('images', IMGPATH))
-nx.write_graphml(g)
+            except Exception as e:
+                print("{}: {}".format(e, row))
+                continue
+
+            # Yield a commit data structure
+            yield Commit(*row)
+
+
+def create_graph(path):
+    """
+    Creates a graph from the data set
+    """
+    g = nx.Graph(name='Sklearn Commits')
+
+    # Make first pass to add commits and parents
+    for commit in parse_dataset(path):
+
+        g.add_node(commit.commit, timestamp=commit.timestamp, type='commit')  # add other elements?
+        g.add_node(commit.contributor, type='contributor')
+        g.add_edge(commit.contributor, commit.commit, label='contributor')
+
+        for parent in commit.parents:
+            g.add_node(parent, type='commit')
+            g.add_edge(parent, commit.commit, label='parent')
+
+    # Make second pass to add time delta weights on parents
+    def elapsed(G, edge):
+        src = G.node[edge[0]]
+        dst = G.node[edge[1]]
+
+        if src['type'] == 'contributor' or dst['type'] == 'contributor':
+            return None
+
+        if 'timestamp' not in src or 'timestamp' not in dst:
+            return 0.0
+
+        return (dst['timestamp'] - src['timestamp']).total_seconds()
+
+    elapsed = partial(elapsed, g)
+    for edge in g.edges():
+        g[edge[0]][edge[1]]['elapsed'] = elapsed(edge)
+
+    return g
+
+
+def write_graphml(g, outpath):
+    """
+    Converts data types then writes the graphml
+    """
+
+    def convert(data):
+        """
+        Converts a dictionary
+        """
+        for key in list(data.keys()):
+            val = data[key]
+
+            if isinstance(val, datetime):
+                data[key] = val.strftime(ISO8601_DATETIME)
+
+            if isinstance(val, type(None)):
+                del data[key]
+
+    # Convert node data types
+    for node in g.nodes():
+        convert(g.node[node])
+
+    # Convert edge data types
+    for edge in g.edges():
+        convert(g[edge[0]][edge[1]])
+
+    nx.write_graphml(g, outpath)
+
+
+if __name__ == '__main__':
+
+    g = create_graph(INPATH)
+    write_graphml(g, GRAPHML)
+    print(nx.info(g))
